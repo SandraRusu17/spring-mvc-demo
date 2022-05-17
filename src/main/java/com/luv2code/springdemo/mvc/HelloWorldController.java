38f2916cecfe5e404f2dc0cf367556e772cf5808
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-05-17 17:16:25 +0300

    Add data to the Spring Model

--- src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
+++ src/main/java/com/luv2code/springdemo/mvc/HelloWorldController.java
@@ -1,7 +1,11 @@
 package com.luv2code.springdemo.mvc;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Locale;
+
 
 @Controller
 public class HelloWorldController {
@@ -18,4 +22,23 @@ public class HelloWorldController {
         return "helloworld";
     }
 
+    // add a new controller method to read the form data and add data to the model
+
+    @RequestMapping("/processFormVersionTwo")
+    public String letsShoutDude(HttpServletRequest request, Model model) {
+
+        // read the request parameter from HTML form
+        String theName = request.getParameter("studentName");
+
+        // convert the data to all caps
+        theName = theName.toUpperCase();
+
+        // create the message
+        String result = "Yo! " + theName;
+
+        // add message to the model
+        model.addAttribute("message", result);
+
+        return "helloworld";
+    }
 }
