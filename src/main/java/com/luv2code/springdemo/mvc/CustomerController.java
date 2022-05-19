e64bc65ccd7fa9b21e068555b2294d897965c7bd
SandraRusu <sandra.rusu.ext@extendaretail.com>
2022-05-19 16:26:31 +0300

    Add Pre-processing Code with @InitBinder

--- src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
+++ src/main/java/com/luv2code/springdemo/mvc/CustomerController.java
@@ -1,8 +1,11 @@
 package com.luv2code.springdemo.mvc;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -11,6 +14,18 @@ import javax.validation.Valid;
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+
+    // add an initbinder ... to convert trim input strings
+    // remove leading and trailing whitespace
+    // resolve issue for our validation
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder) {
+
+        StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+
+        dataBinder.registerCustomEditor(String.class, stringTrimmerEditor);
+    }
 
     @RequestMapping("/showForm")
     public String showForm(Model theModel) {
@@ -23,6 +38,8 @@ public class CustomerController {
     @RequestMapping("/processForm")
     public String processForm(@Valid @ModelAttribute("customer") Customer theCustomer,
                               BindingResult theBindingResult) {
+
+        System.out.println("Last name: |" + theCustomer.getLastName() + "|");
 
         if (theBindingResult.hasErrors()) {
             return "customer-form";
